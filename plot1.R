9d5df9fc531f766304c385b8c669c81690153023
Parhamfard <parham.fard1@gmail.com>
2022-03-19 19:56:25 -0400

    Intial commit

---
+++ plot1.R
@@ -0,0 +1,8 @@
+data_comp <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data <- subset(data_comp, Date %in% c("1/2/2007","2/2/2007"))
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+hist(data$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
